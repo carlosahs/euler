511afd3b1100da30aff04f4925bf29770d55c93f
carlosahs <tacahs12@gmail.com>
2022-01-12 14:56:48 -0600

    Implement TokenSet enum

--- src/lib.rs
+++ src/lib.rs
@@ -6,3 +6,16 @@ mod tests {
         assert_eq!(result, 4);
     }
 }
+
+mod semantics {
+    enum TokenSet {
+        Number,
+        Operator,
+        Invalid,
+    }
+
+    pub struct Digit {
+        token_set: TokenSet,
+        valid: char,
+    }
+}
